65dc6f0fd52efd24725ad63e5529470c9261e904
Root-2 <routledge.dan@gmail.com>
2022-02-15 15:43:35 +0000

    Added functionality and testing, including 404 handling for bad paths.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,9 +10,34 @@ afterAll(() => {db.end() });
 beforeEach(() => seed(data))
 
 describe("api/topics Tests", ()=>{
-    test("api/topics returns a 200 status", ()=>{
+    test("api/topics returns a 200 status and an array of at least one item.", ()=>{
         return request(app)
             .get("/api/topics")
             .expect(200)
+            .then((data)=>{
+                console.log(data.body)
+                expect(Array.isArray(data.body)).toBe(true)
+                expect(data.body.length).toBeGreaterThan(0)
+            })            
+    })
+    test("api/topics items all contain slug and description.", ()=>{
+        return request(app)
+            .get("/api/topics")
+            .expect(200)
+            .then((data)=>{
+                for( let i = 0; i < data.body.length; i++) {
+                    expect(data.body[i].hasOwnProperty('slug')).toBe(true);
+                    expect(data.body[i].hasOwnProperty('description')).toBe(true);
+                }
+            })
+    })
+    test.only("Incorrect APIs return a 404 if the path does not exist.", ()=>{
+        return request(app)
+            .get("/api/thisisabadpath")
+            .expect(404)
+            .then((data)=>{
+                expect(data.text).toEqual("Bad endpoint - File not found.")
+            })
+            
     })
 })
--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -4,10 +4,17 @@ const {fetchTopics} = require("../models/topics.model")
 // Responds with 
 exports.getTopics = (req, res, next) => {
     fetchTopics().then((topics)=> {
-        res.status(200).send({topics})
+        if (topics) {
+        res.status(200).send(topics)
+        } else {
+            throw(404)
+        }
     })
     .catch((err)=>{
         next(err)
     })
 }
 
+exports.badEndpoint = (req, res, next) => {
+    res.status(404).send("Bad endpoint - File not found.")
+}
