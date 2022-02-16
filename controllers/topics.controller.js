69c013a68b144d0a4a2e01b90615f7523d30bd3c
Root-2 <routledge.dan@gmail.com>
2022-02-16 11:56:50 +0000

    Cleaned up a superfluous 'if'

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -4,9 +4,7 @@ const {fetchTopics} = require("../models/topics.model")
 // Responds with 
 exports.getTopics = (req, res, next) => {
     fetchTopics().then((topics)=> {
-        if (topics) {
         res.status(200).send(topics)
-        }
     })
     .catch((err)=>{
         next(err)
