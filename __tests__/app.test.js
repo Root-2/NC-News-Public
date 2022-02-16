09d73eda6b74ce1a0d4f503d3581b83946e89d07
Root-2 <routledge.dan@gmail.com>
2022-02-16 11:27:56 +0000

    Improved testing from feedback.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,13 +1,15 @@
 const request = require('supertest')
 const app = require('../app')
 const seed= require('../db/seeds/seed')
-const data = require('../db/data/test-data')
+const testData = require('../db/data/test-data')
 const db = require('../db/connection')
+
+console.log(testData.topicData)
 
 // Closes database connection after each test.
 afterAll(() => {db.end() });
 // Re-seeds data before each test.
-beforeEach(() => seed(data))
+beforeEach(() => seed(testData))
 
 describe("api/topics Tests", ()=>{
     test("api/topics returns a 200 status and an array of at least one item.", ()=>{
@@ -15,9 +17,8 @@ describe("api/topics Tests", ()=>{
             .get("/api/topics")
             .expect(200)
             .then((data)=>{
-                console.log(data.body)
                 expect(Array.isArray(data.body)).toBe(true)
-                expect(data.body.length).toBeGreaterThan(0)
+                expect(data.body.length).toBe(testData.topicData.length)
             })            
     })
     test("api/topics items all contain slug and description.", ()=>{
@@ -25,19 +26,23 @@ describe("api/topics Tests", ()=>{
             .get("/api/topics")
             .expect(200)
             .then((data)=>{
-                for( let i = 0; i < data.body.length; i++) {
-                    expect(data.body[i].hasOwnProperty('slug')).toBe(true);
-                    expect(data.body[i].hasOwnProperty('description')).toBe(true);
-                }
+                data.body.forEach(item => {
+                    expect.objectContaining({
+                        slug: expect.any(String),
+                        description: expect.any(String)
+                    })
+                })
             })
     })
-    test.only("Incorrect APIs return a 404 if the path does not exist.", ()=>{
+    test("Incorrect APIs return a 404 if the path does not exist.", ()=>{
         return request(app)
             .get("/api/thisisabadpath")
             .expect(404)
             .then((data)=>{
                 expect(data.text).toEqual("Bad endpoint - File not found.")
             })
-            
+            .catch((err)=>{
+                console.log(err)
+            })
     })
 })
